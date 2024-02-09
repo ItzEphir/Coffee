db8664038e1670c0b14295a248dd63906886b97b
Ephir <d.y.dvoryannikov@mail.ru>
2024-02-09 15:26:13 +0300

    09.02.24: feat: add User use cases and models

--- data/src/main/java/com/ephirium/coffee/data/di/DataModule.kt
+++ data/src/main/java/com/ephirium/coffee/data/di/DataModule.kt
@@ -1,10 +1,14 @@
 package com.ephirium.coffee.data.di
 
 import com.ephirium.coffee.data.repository.ComplimentRepositoryImpl
+import com.ephirium.coffee.data.repository.UserRepositoryImpl
 import com.ephirium.coffee.domain.repository.ComplimentRepositoryBase
+import com.ephirium.coffee.domain.repository.UserRepositoryBase
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val dataModule = module {
     singleOf<ComplimentRepositoryBase>(::ComplimentRepositoryImpl)
+    
+    singleOf<UserRepositoryBase>(::UserRepositoryImpl)
 }
